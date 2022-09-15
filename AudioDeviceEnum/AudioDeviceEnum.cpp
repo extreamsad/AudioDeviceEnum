c6c3c4d08eb4d3b1c8f51c7586ad36d06b07d043
extream <extream.sad@mail.ru>
2022-09-15 18:41:14 +0300

    +SetDefaultAudioPlaybackDevice

--- AudioDeviceEnum/AudioDeviceEnum.cpp
+++ AudioDeviceEnum/AudioDeviceEnum.cpp
@@ -2,9 +2,25 @@
 //
 
 #include "stdafx.h"
+#include "PolicyConfig.h"
 
 using namespace std;
 #pragma comment(lib, "Winmm.lib")
+
+HRESULT SetDefaultAudioPlaybackDevice(LPCWSTR devID)
+{
+	IPolicyConfigVista *pPolicyConfig;
+	ERole reserved = eConsole;
+
+	HRESULT hr = CoCreateInstance(__uuidof(CPolicyConfigVistaClient),
+		NULL, CLSCTX_ALL, __uuidof(IPolicyConfigVista), (LPVOID *)&pPolicyConfig);
+	if (SUCCEEDED(hr))
+	{
+		hr = pPolicyConfig->SetDefaultEndpoint(devID, reserved);
+		pPolicyConfig->Release();
+	}
+	return hr;
+}
 
 int main()
 {
--- AudioDeviceEnum/stdafx.h
+++ AudioDeviceEnum/stdafx.h
@@ -11,7 +11,10 @@
 #include <tchar.h>
 #include <windows.h>
 #include <mmsystem.h>
+#include <Propidl.h>
+//#include <Functiondiscoverykeys_devpkey.h>
 #include <iostream>
+#include <mmdeviceapi.h>
 
 
 
